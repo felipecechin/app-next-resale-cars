5e3f4c17b20c48ca694d181a794f9a0b2c329bb7
Felipe Cechin Mello <ficechin@hotmail.com>
2022-10-28 08:30:48 -0300

    adjust on ApexColumnChart

--- src/components/shared/charts/apexcharts/ApexColumnChart/index.tsx
+++ src/components/shared/charts/apexcharts/ApexColumnChart/index.tsx
@@ -8,15 +8,13 @@ interface ApexColumnChartProps {
     categoriesKey: string;
     dataKey: string;
     id: string;
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    onClickColumn?: (event: any, chartContext: any, config: any) => void;
     height: string[];
     dataName: string;
     xAxisLabelsOptions?: object;
 }
 
 
-function ApexColumnChart({ data, categoriesKey, dataKey, id, onClickColumn, height, dataName, xAxisLabelsOptions = {} }: ApexColumnChartProps): JSX.Element {
+function ApexColumnChart({ data, categoriesKey, dataKey, id, height, dataName, xAxisLabelsOptions = {} }: ApexColumnChartProps): JSX.Element {
     const [series, setSeries] = useState<ApexAxisChartSeries>([]);
     const [options, setOptions] = useState({});
 
@@ -44,14 +42,6 @@ function ApexColumnChart({ data, categoriesKey, dataKey, id, onClickColumn, heig
         const options = {
             chart: {
                 id: id || 'basic-chart',
-                events: {
-                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-                    dataPointSelection: function (event: any, chartContext: any, config: any) {
-                        if (onClickColumn) {
-                            onClickColumn(event, chartContext, config);
-                        }
-                    }
-                },
                 height: height ? height[0] : 'auto',
                 toolbar: {
                     show: false,
