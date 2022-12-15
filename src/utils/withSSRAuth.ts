ca3eb7f4a71d80fd15ab3d3b655caa4927ae5326
Felipe Cechin Mello <ficechin@hotmail.com>
2022-12-15 11:27:53 -0300

    updated withSSRAuth

--- src/utils/withSSRAuth.ts
+++ src/utils/withSSRAuth.ts
@@ -1,26 +1,20 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import {
-    GetServerSideProps,
-    GetServerSidePropsContext,
-    GetServerSidePropsResult,
-    PreviewData,
-} from 'next'
+import { GetServerSideProps, GetServerSidePropsContext, GetServerSidePropsResult, PreviewData } from 'next'
 
 import { ParsedUrlQuery } from 'querystring'
 import { getToken } from '@/utils/cookies'
 
-interface IPagePropsWithToken
-    extends GetServerSidePropsContext<ParsedUrlQuery, PreviewData> {
+interface IPagePropsWithToken extends GetServerSidePropsContext<ParsedUrlQuery, PreviewData> {
     token: string
 }
 
-type TGetServerSidePropsWithToken<
-    P extends { [key: string]: any } = { [key: string]: any }
-> = (context: IPagePropsWithToken) => Promise<GetServerSidePropsResult<P>>
+type TGetServerSidePropsWithToken<P extends { [key: string]: any } = { [key: string]: any }> = (
+    context: IPagePropsWithToken
+) => Promise<GetServerSidePropsResult<P>>
 
 export function withSSRAuth<P extends { [key: string]: any }>(
     fn: TGetServerSidePropsWithToken<P>
-): GetServerSideProps {
+): GetServerSideProps<P> {
     return async (
         ctx: GetServerSidePropsContext<ParsedUrlQuery, PreviewData>
     ): Promise<GetServerSidePropsResult<P>> => {
@@ -29,8 +23,7 @@ export function withSSRAuth<P extends { [key: string]: any }>(
         if (!token) {
             return {
                 redirect: {
-                    destination:
-                        '/auth?redirect=' + encodeURIComponent(ctx.resolvedUrl),
+                    destination: '/auth?redirect=' + encodeURIComponent(ctx.resolvedUrl),
                     permanent: false,
                 },
             }
